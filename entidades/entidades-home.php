6dc034c6b18a962e9c44cb5eb804efff97ce9f81
Andres <polachgg@gmail.com>
2017-04-10 09:06:33 -0300

    *ASAS
    sASAs

--- entidades/entidades-home.php
+++ entidades/entidades-home.php
@@ -14,7 +14,9 @@
   include ('lib.php');
   include ('checks.php');
   include ('conect.php');
- 
+ if ($_SESSION) {
+   # code...
+ }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
